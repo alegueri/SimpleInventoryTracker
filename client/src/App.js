7bd65faac064326a5e2e54d54b674803eb1d1e7e
alegueri <ale.guerinoni@gmail.com>
2022-01-19 16:55:46 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import CSVReport from './components/CSVReport';
 
 // The combinations of all modules get called here. This is the main page. 
 function App() {
-  const {iItems, newiItems, setiItems, setnewiItems, setNewItem, setNewPrice, setNewQuantity, itemList, showClicked, handleChange,handleChange2, addItem, getItemList, deleteItem, updateItem}=HandleStates();
+  const {iItems, newiItems, setiItems, setnewiItems, itemList, showClicked, handleChange,handleChange2, addItem, getItemList, deleteItem, updateItem}=HandleStates();
 
   return (
     <div style={{display:'flex', flexDirection:'column',alignItems:'center'}}> 
